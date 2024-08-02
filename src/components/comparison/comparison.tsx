72d81a067d81c356af53e24315210cc541190bbe
Hashir Ahmed <hashirahmedaurangzeb@gmail.com>
2024-08-02 15:42:36 +0500

    .

--- src/components/comparison/comparison.tsx
+++ src/components/comparison/comparison.tsx
@@ -24,7 +24,8 @@ const Comparison = ({ selectedValues }: ComparisonProps) => {
 
       try {
         const packagesData = await getPackageData(selectedValues);
-        
+        console.log('Fetched data:', packagesData);
+
         const [pkg1, pkg2] = [packagesData[selectedValues[0]], packagesData[selectedValues[1]]];
 
         if (!pkg1 || !pkg2) {
